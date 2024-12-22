31f591cddf5cafda1275200010fef03ff647e1e0
officefish <fusikshukovky@gmail.com>
2024-12-22 21:12:26 +0300

    close buttons added

--- src/pages/bawdry.tsx
+++ src/pages/bawdry.tsx
@@ -10,6 +10,7 @@ import { useOneRoundBawdry } from "@/hooks/api/fortunes/useOneRoundBawdry";
 import { useBalanceStore } from "@/providers/balance";
 import { useFortuneStore } from "@/providers/fortunes";
 import { useDeleteAction } from "@/hooks/api/actions/useDeleteAction";
+import { useNavigate } from "react-router-dom";
 
 const Bawdry: FC = () => {
 
@@ -92,9 +93,25 @@ const Bawdry: FC = () => {
 
   const { gems } = useBalanceStore(); 
 
+  const navigate = useNavigate();
+  const handleClose = () => {
+    navigate('/');
+  }
+
   return (
     <div className='w-full'>
       <div className="absolute h-screen w-screen bawdry-bg top-0 vignette"></div>
+
+
+      <div className="absolute top-20 right-8 z-10 btn-no-body opacity-40 hover:opacity-90"
+      onClick={handleClose}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+          <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM8.29289 8.29289C8.68342 7.90237 9.31658 7.90237 9.70711 8.29289L12 10.5858L14.2929 8.29289C14.6834 7.90237 15.3166 7.90237 15.7071 8.29289C16.0976 8.68342 16.0976 9.31658 15.7071 9.70711L13.4142 12L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L12 13.4142L9.70711 15.7071C9.31658 16.0976 8.68342 16.0976 8.29289 15.7071C7.90237 15.3166 7.90237 14.6834 8.29289 14.2929L10.5858 12L8.29289 9.70711C7.90237 9.31658 7.90237 8.68342 8.29289 8.29289Z" 
+          fill="#f71fde"/>
+        </svg>
+      </div>
+
       { checked ? (
         <div className="
         w-full absolute top-32 bg-[#ecbde7] bg-opacity-80 
--- src/pages/lantern.tsx
+++ src/pages/lantern.tsx
@@ -108,7 +108,7 @@ const Lantern: FC = () => {
       <div className="absolute w-full h-screen lantern-bg top-0 vignette overflow-hidden"></div>
 
       {/* Close button */}
-      <div className="absolute top-20 right-10 z-10 btn-no-body opacity-40 hover:opacity-90"
+      <div className="absolute top-20 right-8 z-10 btn-no-body opacity-40 hover:opacity-90"
       onClick={handleClose}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
